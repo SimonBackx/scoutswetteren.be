09808243fdbfc7b4839eed0f9154d82b50552b85
Simon Backx <simonbbackx@gmail.com>
2016-08-23 18:20:44 +0200

    Gegevens aanpassen pagina

--- pirate/sails/leiding/admin/gegevens.php
+++ pirate/sails/leiding/admin/gegevens.php
@@ -3,6 +3,7 @@ namespace Pirate\Sail\Leiding\Admin;
 use Pirate\Page\Page;
 use Pirate\Block\Block;
 use Pirate\Template\Template;
+use Pirate\Model\Leiding\Leiding;
 
 class Gegevens extends Page {
     function getStatusCode() {
@@ -10,12 +11,68 @@ class Gegevens extends Page {
     }
 
     function getContent() {
-        return '<h1>Hier kunnen gegevens bewerkt worden (later).</h1>';
-        /*return Template::render('leiding/admin', array(
-            'content' => $this->adminPage->getContent(),
-            'admin' => array(
-                'selected' => $this->selected
-            )
-        ));*/
+        $permissions = Leiding::getPermissions();
+        $user = Leiding::getUser();
+
+        $errors = array();
+        $success = false;
+
+        $data = array(
+            'firstname' => $user->firstname,
+            'lastname' => $user->lastname,
+            'totem' => $user->totem,
+            'mail' => $user->mail,
+            'phone' => $user->phone,
+        );
+
+        if (isset($_POST['firstname'], $_POST['lastname'], $_POST['totem'], $_POST['mail'], $_POST['phone'])) {
+            $data['firstname'] = $_POST['firstname'];
+            $data['lastname'] = $_POST['lastname'];
+            $data['totem'] = $_POST['totem'];
+            $data['mail'] = $_POST['mail'];
+            $data['phone'] = $_POST['phone'];
+
+            // Nu één voor één controleren
+            $errors = $user->setProperties($data);
+
+            if (count($errors) == 0) {
+                if ($user->save())
+                    $success = true;
+                else
+                    $errors[] = 'Probleem bij opslaan';
+            }
+        }
+
+        $functies = array();
+        foreach ($permissions as $permission) {
+            switch ($permission) {
+                case 'leiding':
+                    if (empty($user->tak)) {
+                        $functies[] = 'Leiding';
+                    } else {
+                        $functies[] = 'Leiding ('.$user->tak.')';
+                    }
+                    break;
+                
+                case 'oudercomite':
+                    $functies[] = 'Oudercomité';
+                    break;
+
+                case 'financieel':
+                    $functies[] = 'Financieel verantwoordelijke';
+                    break;
+
+                default :
+                    $functies[] = ucfirst($permission);
+                break;
+            }
+        }
+
+        return Template::render('leiding/gegevens', array(
+            'leiding' => $data,
+            'functies' => $functies,
+            'errors' => $errors,
+            'success' => $success
+        ));
     }
 }
--- pirate/sails/leiding/models/leiding.php
+++ pirate/sails/leiding/models/leiding.php
@@ -238,6 +238,123 @@ class Leiding extends Model {
         return crypt($password, $salt);
     }
 
+    // Controle
+    // 
+    function isValidFirstname($firstname) {
+        $pattern = '/^[\w ]+$/';
+        return (preg_match($pattern, $firstname) === 1);
+    }
+
+    function isValidLastname($lastname) {
+        $pattern = '/^[\w ]+$/';
+        return (preg_match($pattern, $lastname) === 1);
+    }
+
+    function isValidTotem($totem) {
+        $pattern = '/^[\w ]+$/';
+        return (preg_match($pattern, $totem) === 1);
+    }
+
+    function isValidPhone($phone) {
+        $pattern = '/^[0-9 +()]+$/';
+        return (preg_match($pattern, $phone) === 1);
+    }
+
+    function isValidMail($mail) {
+        return (filter_var($mail, FILTER_VALIDATE_EMAIL));
+    }
+
+    // empty array on success
+    // array of errors on failure
+    function setProperties(&$data) {
+        $errors = array();
+
+        if ($this->isValidFirstname($data['firstname'])) {
+            $this->firstname = $data['firstname'];
+        } else {
+            $errors[] = 'Ongeldige voornaam';
+        }
+
+        if ($this->isValidLastname($data['lastname'])) {
+            $this->lastname = $data['lastname'];
+        } else {
+            $errors[] = 'Ongeldige achternaam';
+        }
+
+        if ($this->isValidTotem($data['totem'])) {
+            $this->totem = $data['totem'];
+        }  else {
+            $errors[] = 'Ongeldige totem';
+        }
+
+        if ($this->isValidMail($data['mail'])) {
+            $this->mail = $data['mail'];
+        }  else {
+            $errors[] = 'Ongeldige e-mailadres';
+        }
+
+        if ($this->isValidPhone($data['phone'])) {
+            $output = preg_replace('/[^0-9+]/', '', $data['phone']);
+
+            // lengte bepalen
+            if (strlen($output) != 10 && strlen($output) != 12 && strlen($output) != 13) {
+                $errors[] = 'Lengte - Vul GSM nummer in formaat +324 XX XX XX XX of 04XX XX XX XX';
+            } else {
+                $original = $output;
+                $error = false;
+                if (substr($output, 0, 4) == '0032' && strlen($output) == 13) {
+                    $output = substr($output, 2);
+                }
+
+                if (substr($output, 0, 3) == '+32' && strlen($output) == 12) {
+                    $output = substr($output, 1);
+                }
+
+                if (substr($output, 0, 3) != '324' || (strlen($original) != 12 && strlen($original) != 13)) {
+                    if (substr($output, 0, 2) != '04') {
+                        $errors[] = 'Vul GSM nummer in formaat +324 XX XX XX XX of 04XX XX XX XX';
+                        $error = true;
+                    } else {
+                        $output = '32'.substr($output, 1);
+                    }
+                }
+
+                if (!$error) {
+                    $output = '+'.$output;
+                    $this->phone = $output;
+                    $data['phone'] = $this->phone;
+                }
+
+            }
+
+
+        } else {
+            $errors[] = 'Ongeldig GSM nummer';
+        }
+        return $errors;
+    }
+
+    function save(){
+        $id = self::getDb()->escape_string($this->id);
+        $firstname = self::getDb()->escape_string($this->firstname);
+        $lastname = self::getDb()->escape_string($this->lastname);
+        $totem = self::getDb()->escape_string($this->totem);
+        $mail = self::getDb()->escape_string($this->mail);
+        $phone = self::getDb()->escape_string($this->phone);
+
+        $query = "UPDATE leiding 
+            SET 
+             firstname = '$firstname',
+             lastname = '$lastname',
+             totem = '$totem',
+             mail = '$mail',
+             phone = '$phone'
+             where id = '$id' 
+        ";
+
+        return self::getDb()->query($query);
+    }
+
 
     
 }
