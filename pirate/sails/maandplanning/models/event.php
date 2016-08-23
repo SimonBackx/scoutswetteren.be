5ca079d7aba1dae68650ed375f607300c5e87d90
Simon Backx <simonbbackx@gmail.com>
2016-08-24 01:26:25 +0200

    Fixed event overview query

--- pirate/sails/maandplanning/models/event.php
+++ pirate/sails/maandplanning/models/event.php
@@ -10,7 +10,7 @@ class Event extends Model {
     public $location;
     public $endlocation;
     public $group;
-    public $in_past;
+    public $in_past = false;
 
     static private $groups = array('Kapoenen', 'Wouters', 'Jonggivers', 'Givers', 'Jin', 'Leiding', 'Oudercomité', 'Alle takken', 'Familie en vrienden');
 
@@ -23,7 +23,9 @@ class Event extends Model {
         $this->location = $row['location'];
         $this->endlocation = $row['endlocation'];
         $this->group = $row['group'];
-        $this->in_past = $row['in_past'];
+
+        if (isset($row['in_past']))
+            $this->in_past = $row['in_past'];
     }
 
     // Maximaal 30 events! Rest wordt weg geknipt
@@ -32,7 +34,7 @@ class Event extends Model {
         $enddate = self::getDb()->escape_string($enddate);
 
         $events = array();
-        $query = 'SELECT *, case when startdate < NOW() then 1 else 0 end as in_past FROM events WHERE (startdate >= "'.$startdate.'" AND startdate < "'.$enddate.'") OR (enddate >= "'.$startdate.'" AND enddate < "'.$enddate.'") ORDER BY startdate, group_order LIMIT 30';
+        $query = 'SELECT *, case when startdate < CURDATE() then 1 else 0 end as in_past FROM events WHERE (startdate >= "'.$startdate.'" AND startdate < "'.$enddate.'") OR (enddate >= "'.$startdate.'" AND enddate < "'.$enddate.'") ORDER BY startdate, group_order LIMIT 30';
         if ($result = self::getDb()->query($query)){
             if ($result->num_rows>0){
                 while ($row = $result->fetch_assoc()) {
@@ -47,7 +49,7 @@ class Event extends Model {
         $needle = self::getDb()->escape_string($needle);
         
         $events = array();
-        $query = 'SELECT *, case when startdate < NOW() then 1 else 0 end as in_past FROM events WHERE `name` like "%'.$needle.'%" OR `group` like "%'.$needle.'%" ORDER BY in_past, startdate, group_order LIMIT 30';
+        $query = 'SELECT *, case when startdate < CURDATE() then 1 else 0 end as in_past FROM events WHERE `name` like "%'.$needle.'%" OR `group` like "%'.$needle.'%" ORDER BY in_past, startdate, group_order LIMIT 30';
         
         if ($result = self::getDb()->query($query)){
             if ($result->num_rows>0){
@@ -63,7 +65,7 @@ class Event extends Model {
         $tak = self::getDb()->escape_string($tak);
 
         $events = array();
-        $query = 'SELECT * FROM events WHERE startdate >= NOW() AND group = "'.ucfirst($tak).'" OR group = "Familie en vrienden" OR group = "Alle takken" ORDER BY startdate LIMIT 30';
+        $query = 'SELECT * FROM events WHERE startdate >= CURDATE() AND (`group` = "'.ucfirst($tak).'" OR `group` = "Familie en vrienden" OR `group` = "Alle takken") ORDER BY startdate LIMIT 30';
 
         if ($result = self::getDb()->query($query)){
             if ($result->num_rows>0){
