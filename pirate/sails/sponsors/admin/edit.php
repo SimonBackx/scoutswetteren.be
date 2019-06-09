c8f9ba27cfaf7775063ae3dc358e98594e37d717
Simon Backx <hi@simonbackx.com>
2019-06-09 16:49:41 +0200

    Sponsor improvements

--- pirate/sails/sponsors/admin/edit.php
+++ pirate/sails/sponsors/admin/edit.php
@@ -1,24 +1,28 @@
 <?php
 namespace Pirate\Sail\Sponsors\Admin;
-use Pirate\Page\Page;
-use Pirate\Block\Block;
-use Pirate\Template\Template;
-use Pirate\Model\Files\Image;
-use Pirate\Model\Files\File;
-use Pirate\Model\Sponsors\Sponsor;
 
-class Edit extends Page {
+use Pirate\Model\Files\File;
+use Pirate\Model\Files\Image;
+use Pirate\Model\Sponsors\Sponsor;
+use Pirate\Page\Page;
+use Pirate\Template\Template;
+
+class Edit extends Page
+{
     private $sponsor = null;
 
-    function __construct($sponsor = null) {
+    public function __construct($sponsor = null)
+    {
         $this->sponsor = $sponsor;
     }
 
-    function getStatusCode() {
+    public function getStatusCode()
+    {
         return 200;
     }
 
-    function getContent() {
+    public function getContent()
+    {
         // Geen geldig id = nieuwe sponsor toevoegen
         $new = !isset($this->sponsor);
         $errors = array();
@@ -30,7 +34,7 @@ class Edit extends Page {
 
         $data = array(
             'name' => $this->sponsor->name,
-            'url' => $this->sponsor->url
+            'url' => $this->sponsor->url,
         );
 
         $allset = true;
@@ -39,7 +43,7 @@ class Edit extends Page {
                 $allset = false;
                 break;
             }
-            
+
             $data[$key] = $_POST[$key];
         }
 
@@ -47,12 +51,11 @@ class Edit extends Page {
             $this->sponsor->name = $data['name'];
             $this->sponsor->url = $data['url'];
 
-
             if (count($errors) == 0) {
                 $form_name = "logo";
                 if (File::isFileSelected($form_name)) {
                     $img = new Image();
-                    if ($img->upload($form_name, array(array('height' => 100)), $errors, null, true)) {
+                    if ($img->upload($form_name, array(array('height' => 160 * 2)), $errors, null, true)) {
                         $this->sponsor->image = $img;
                     }
                 }
@@ -76,7 +79,7 @@ class Edit extends Page {
             'data' => $data,
             'sponsor' => $this->sponsor,
             'errors' => $errors,
-            'success' => $success
+            'success' => $success,
         ));
     }
 }
