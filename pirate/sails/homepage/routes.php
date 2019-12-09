9abd4a05e4bf2d053cec70d30578a5ab59db5436
Simon Backx <hi@simonbackx.com>
2019-12-09 07:59:34 +0100

    Fixed: scoutswetteren homepage router

--- pirate/sails/homepage/routes.php
+++ pirate/sails/homepage/routes.php
@@ -1,14 +1,13 @@
 <?php
 namespace Pirate\Sails\Homepage;
-use Pirate\Wheel\Page;
+
 use Pirate\Wheel\Route;
 
-class HomepageRouter extends Route {
-    function doMatch($url, $parts) {
+class HomepageRouter extends Route
+{
+    public function doMatch($url, $parts)
+    {
         if ($url == '') {
-            return true;
-        }
-        if ($url == 'sponsors') {
             return true;
         }
 
@@ -18,17 +17,13 @@ class HomepageRouter extends Route {
         return false;
     }
 
-    function getPage($url, $parts) {
-        if ($url == 'sponsors') {
-            require(__DIR__.'/pages/sponsors.php');
-            return new Pages\Sponsors();
-        }
-
+    public function getPage($url, $parts)
+    {
         if ($url == 'privacy') {
-            require(__DIR__.'/pages/privacy.php');
+            require __DIR__ . '/pages/privacy.php';
             return new Pages\Privacy();
         }
-        require(__DIR__.'/pages/homepage.php');
+        require __DIR__ . '/pages/homepage.php';
         return new Pages\Homepage();
     }
 }
