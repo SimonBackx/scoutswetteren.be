b60e624dbd7a0170c680f599beb4d8deb3d8104a
Simon Backx <hi@simonbackx.com>
2019-12-09 08:05:25 +0100

    Added redacteur sponsor

--- pirate/sails/sponsors/admin.php
+++ pirate/sails/sponsors/admin.php
@@ -1,39 +1,45 @@
 <?php
 namespace Pirate\Sails\Sponsors;
-use Pirate\Wheel\Page;
-use Pirate\Wheel\AdminRoute;
-use Pirate\Sails\Sponsors\Models\Sponsor;
-use Pirate\Sails\Leiding\Models\Leiding;
 
-class SponsorsAdminRouter extends AdminRoute {
+use Pirate\Sails\Leiding\Models\Leiding;
+use Pirate\Sails\Sponsors\Models\Sponsor;
+use Pirate\Wheel\AdminRoute;
+
+class SponsorsAdminRouter extends AdminRoute
+{
     private $sponsor = null;
 
-    static function getAvailablePages() {
+    public static function getAvailablePages()
+    {
         return [
             'oudercomite' => array(
-                array('name' => 'Sponsors', 'url' => 'sponsors')
+                array('name' => 'Sponsors', 'url' => 'sponsors'),
             ),
             'groepsleiding' => array(
-                array('name' => 'Sponsors', 'url' => 'sponsors')
+                array('name' => 'Sponsors', 'url' => 'sponsors'),
+            ),
+            'redacteur' => array(
+                array('name' => 'Sponsors', 'url' => 'sponsors'),
             ),
         ];
     }
 
-    function doMatch($url, $parts) {
+    public function doMatch($url, $parts)
+    {
         if (!Leiding::hasPermission('sponsors')) {
             return false;
         }
-        
-        if (count($parts) >=2 && count($parts) <= 3 && $parts[0] == 'sponsors' && $parts[1] == 'edit') {
+
+        if (count($parts) >= 2 && count($parts) <= 3 && $parts[0] == 'sponsors' && $parts[1] == 'edit') {
             if (isset($parts[2])) {
                 if (!is_numeric($parts[2])) {
                     return false;
                 }
-                
+
                 $this->sponsor = Sponsor::getSponsor(intval($parts[2]));
                 return isset($this->sponsor);
             }
-            
+
             // New
             return true;
         }
@@ -42,7 +48,7 @@ class SponsorsAdminRouter extends AdminRoute {
             if (!is_numeric($parts[2])) {
                 return false;
             }
-            
+
             $this->sponsor = Sponsor::getSponsor(intval($parts[2]));
             return isset($this->sponsor);
         }
@@ -54,18 +60,19 @@ class SponsorsAdminRouter extends AdminRoute {
         return false;
     }
 
-    function getPage($url, $parts) {
+    public function getPage($url, $parts)
+    {
         if (isset($parts[1]) && $parts[1] == 'edit') {
-            require(__DIR__.'/admin/edit.php');
+            require __DIR__ . '/admin/edit.php';
             return new Admin\Edit($this->sponsor);
         }
 
         if (isset($parts[1]) && $parts[1] == 'delete') {
-            require(__DIR__.'/admin/delete.php');
+            require __DIR__ . '/admin/delete.php';
             return new Admin\Delete($this->sponsor);
         }
 
-        require(__DIR__.'/admin/overview.php');
+        require __DIR__ . '/admin/overview.php';
         return new Admin\Overview();
     }
 }
