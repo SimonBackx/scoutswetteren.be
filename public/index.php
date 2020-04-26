d40b312ba12200e195c8409ab4c05fe1d8faa859
Simon Backx <hi@simonbackx.com>
2020-04-26 12:46:37 +0200

    Docker fixes + 75 jaar

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
+if (isset($_SERVER['DEBUG'])) {
+    // Env variable moved to server randomly without a reason... Can't find the issue.
+    $_ENV['DEBUG'] = $_SERVER['DEBUG'];
+}
 
 require __DIR__ . '/../pirate/run/http.php';
